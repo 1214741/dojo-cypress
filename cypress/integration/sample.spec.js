945e6e705a825224a12ef1654f35130777eef087
1214741 <j98fer@gmail.com>
2022-04-02 06:50:01 -0700

    first commit

---
+++ cypress/integration/sample.spec.js
@@ -0,0 +1,12 @@
+describe('My First Cypress Test', () => {
+    it('Assert Example', () => {
+      expect(true).to.equal(true)
+    })
+
+    it('Visits cypress test webpage ', () => {
+      cy.visit('https://example.cypress.io/')
+      cy.contains('type').click()
+      cy.url().should('include','/commands/actions')
+      
+    })
+})
