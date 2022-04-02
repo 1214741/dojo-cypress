270827c9491324dc7855eb0d256200d616c63c38
1214741 <j98fer@gmail.com>
2022-04-02 07:04:58 -0700

    number and email

--- cypress/integration/sample.spec.js
+++ cypress/integration/sample.spec.js
@@ -7,6 +7,13 @@ describe('My First Cypress Test', () => {
       cy.visit('https://example.cypress.io/')
       cy.contains('type').click()
       cy.url().should('include','/commands/actions')
-      
+
+      cy.get('.action-email')
+  .type('jfernando.tamayo@udea.edu.co').should('have.value', 'jfernando.tamayo@udea.edu.co')
+
+  cy.get('.action-form')
+  .find('[type="text"]').type('123456789')
+  cy.get('.action-form').submit()
+  .next().should('contain', 'Your form has been submitted!')
     })
 })
